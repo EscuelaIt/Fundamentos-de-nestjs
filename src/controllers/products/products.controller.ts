d40b319ee624354640eb99e440a0f51287a499f7
midesweb <malvarez@desarrolloweb.com>
2021-10-06 12:54:39 +0200

    request con distintos métodos http

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { 
+  Body, 
+  Controller, 
+  Delete, 
+  Get, 
+  Param, 
+  Post, 
+  Put 
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -15,5 +23,32 @@ export class ProductsController {
   @Get(':id')
   find(@Param('id') id: number) {
     return `El producto que quieres recibir es ${id}`;
+  }
+
+  // @Post()
+  // create(@Body() body) {
+  //   return `Estás creando un producto ${body.name} con el texto ${body.description}`;
+  // }
+
+  @Post()
+  create(
+    @Body('name') name: string, 
+    @Body('description') description: string
+  ) {
+    return `Estás creando un producto ${name} con el texto ${description}.`;
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body('name') name: string,
+    @Body('description') description: string
+  ) {
+    return `Estas actualizando el producto ${id}, colocando los datos ${name} con texto ${description}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return `Estás borrando el producto ${id}`;
   }
 }
