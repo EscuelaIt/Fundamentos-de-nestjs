40107f6940ae87186eddb21027a86dc4cd3afc34
midesweb <malvarez@desarrolloweb.com>
2021-10-07 10:15:09 +0200

    mover carpeta de products

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './controllers/products/products.controller';
+import { ProductsController } from './products/products.controller';
 
 @Module({
   imports: [],
