e8388271a35ab4ab733ce884cb9ed1eea5d5c5c4
midesweb <malvarez@desarrolloweb.com>
2021-10-13 16:42:52 +0200

    tags

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Tag } from './tag.interface';
+import { TagsService } from './tags.service';
 
 @Controller('tags')
-export class TagsController {}
+export class TagsController {
+
+  constructor(private readonly tagsService: TagsService) {}
+
+  @Get()
+  getAll(): Tag[] {
+    return this.tagsService.getAll();
+  }
+}
--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Tag } from './tag.interface';
 
 @Injectable()
-export class TagsService {}
+export class TagsService {
+  private tags: Tag[] = [
+    {
+      id: 1,
+      name: 'Muebles',
+      slug: 'muebles'
+    },
+    {
+      id: 2,
+      name: 'Accesorios',
+      slug: 'accesorios'
+    }, 
+  ];
+
+  getAll() :Tag[] {
+    return this.tags;
+  }
+}
