6c8b74f395bfcc1f3b3e34aa4c961b3bb6170898
midesweb <malvarez@desarrolloweb.com>
2021-10-14 15:25:21 +0200

    modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
-import { TagsModule } from './tags/tags.module';
+import { ProductsController } from './products/products.controller';
+import { ProductsService } from './products/products.service';
 
 @Module({
-  imports: [ProductsModule, TagsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [],
+  controllers: [AppController, ProductsController],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
