7880eefc2deeef37aab575c44acd6a087d3181b9
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2022-01-13 00:02:43 +0100

    cambiar user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { TagsModule } from './tags/tags.module';
-// import { Product } from './products/entities/product.entity';
 
 @Module({
   imports: [ProductsModule, TagsModule, TypeOrmModule.forRoot({
     type: 'mysql',
     host: '192.168.10.10',
     port: 3306,
-    username: 'homestead',
+    username: 'nest',
     password: 'secret',
     database: 'curso_nest',
     retryDelay: 3000,
