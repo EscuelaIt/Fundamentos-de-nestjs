90eb90ea52675f6e7b19d95038c550593f251f1b
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2022-03-31 01:45:40 +0200

    configurar swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,14 @@ async function bootstrap() {
     disableErrorMessages: true,
     transform: true,
   }));
+  const config = new DocumentBuilder()
+    .setTitle('products example')
+    .setDescription('The products API description')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
--- src/products/dto/product.dto.ts
+++ src/products/dto/product.dto.ts
@@ -1,15 +1,19 @@
 import { IsInt, IsString, Length, Min } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ProductDto {
+  @ApiProperty()
   @IsString({
     message: 'El nombre se ha de indicar'
   })
   name: string;
 
+  @ApiProperty()
   @IsString()
   @Length(10, 50)
   description: string;
 
+  @ApiProperty()
   @IsInt()
   @Min(0, { message: 'El stock puede ser cero o superior'})
   stock: number;
